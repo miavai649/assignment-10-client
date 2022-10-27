b1a008c3c2251545e44398a09afc621be450460c
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-10-27 10:49:20 +0600

    static card added

--- src/CourseCard/CourseCard.js
+++ src/CourseCard/CourseCard.js
@@ -1,27 +1,23 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
-const CourseCard = () => {
+const CourseCard = ({course}) => {
+  const { title } = course;
   return (
-    <div>
-      <Row xs={1} md={3} className="g-4">
-          <Col>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-      </Row>
-    </div>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
   );
 };
 
--- src/Courses/Courses.js
+++ src/Courses/Courses.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Row } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
 import CourseCard from "../CourseCard/CourseCard";
 
 const Courses = () => {
+  const courses = useLoaderData();
   return (
     <div>
       <div class="row container mx-auto">
-        <div class="col-4">col-4</div>
-        <div class="col-8" style={{marginTop: '80px'}}>
-            <CourseCard></CourseCard>
+        <div class="col-8" style={{ marginTop: "80px" }}>
+          <Row  sm={1} md={2}  className="g-4">
+          {
+            courses.map(course => <CourseCard key={course.id} course={course}></CourseCard>)
+          }
+          </Row>
         </div>
+        <div class="col-4">col-4</div>
       </div>
     </div>
   );
